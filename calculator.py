976727be23d9cac331468540e52b51e2ef51a9c7
MD SAMIUR RAHMAN TANIM <86911763+MSRTanim@users.noreply.github.com>
2023-07-04 15:01:41 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,46 @@
+# a = input()
+# print(a)
+
+b = input ("Enter your first Name: ")
+# print(b)
+# print("My Name is ", b)
+
+c = input("Enter your Last Name: ")
+# print("My Name is ", c)
+print("\n")
+print("My Name is ", b + c)
+ # b + c = bc; can't make space
+
+print("\n")
+
+d = input("Enter your first Number (Example: 1, 2, 3, 4... Only Real Number): ")
+e = input("Enter your second Number (Example: 1, 2, 3, 4...Only Real Number)")
+print("\n")
+# Alt + shift + Down Arrow for replicate line.
+# Click Alt to make multiple cursor.
+
+print("Addition of your number is", int(d) + int(e))
+
+print("\n")
+
+print("Subtraction of your number is", int(d) - int(e))
+
+print("\n")
+
+print("Multiplication of your number is", int(d) * int(e))
+
+print("\n")
+
+print("Expotential of your number is", int(d) ** int(e))
+
+print("\n")
+
+print("Division of your number is", int(d) / int(e))
+
+print("\n")
+
+print("Modulus of your number is", int(d) % int(e))
+
+print("\n")
+
+print("Floor Division of your number is", int(d) // int(e))
